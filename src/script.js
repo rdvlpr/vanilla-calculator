aeb58fc40fe5111025212a27c38acf4c7255132e
slndrbbe <125832173+slndrbbe@users.noreply.github.com>
2023-07-03 17:35:38 +0100

    Added button functionality

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function displayNumber() {
+  display = document.getElementById("display");
+  display.value = "7";
+}
+let seven = document.getElementById("seven");
+seven.addEventListener("click", displayNumber);
