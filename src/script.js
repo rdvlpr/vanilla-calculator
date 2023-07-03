378ee2e196f7a2476a8a35dc71e080d5bf65d95f
slndrbbe <125832173+slndrbbe@users.noreply.github.com>
2023-07-03 19:23:54 +0100

    Added signature

--- src/script.js
+++
@@ -1,6 +0,0 @@
-function displayNumber() {
-  display = document.getElementById("display");
-  display.value = "7";
-}
-let seven = document.getElementById("seven");
-seven.addEventListener("click", displayNumber);
